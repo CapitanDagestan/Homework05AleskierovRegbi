08fe33ddc16326c656d5b7fb3c6f8c611645aa61
CapitanDagestan <aleskerovbogdan@gmail.com>
2023-08-08 20:57:50 +0300

    Fix for JDK 1.8

--- src/Regbi.java
+++ src/Regbi.java
@@ -3,14 +3,18 @@ import java.util.Random;
 public class Regbi {
     public static void main(String[] args) {
         Random random = new Random();
+        int[] age = new int[23];
+        for (int a = 0; a<=22; a++){
+            age[a] = 18 + a;
+        }
         int[] comA = new int[25];
         int[] comB = new int[25];
         double sumComA = 0;
         double sumComB = 0;
         System.out.print("Вік гравців команди \"Headless abobs\": ");
         for (int i = 0; i <= 24; i++) {
-            int r = random.nextInt(18, 41);
-            comA[i] = r;
+            int r = random.nextInt(23);
+            comA[i] = age[r];
             if (i < 24) {
                 System.out.print(comA[i] + ", ");
             } else if (i == 24) {
@@ -19,12 +23,12 @@ public class Regbi {
             sumComA += comA[i];
         }
         double midComA = sumComA/comA.length;
-        System.out.println("Середній вік гравців команди \"Headless abobs\": " + midComA + ".");
+        System.out.println("Середній вік гравців: " + midComA + ".");
 
         System.out.print("Вік гравців команди \"Godzillas\": ");
         for (int j = 0; j <= 24; j++) {
-            int r = random.nextInt(18, 41);
-            comB[j] = r;
+            int r = random.nextInt(23);
+            comB[j] = age[r];
             if (j < 24) {
                 System.out.print(comB[j] + ", ");
             } else if (j == 24) {
@@ -33,7 +37,7 @@ public class Regbi {
             sumComB += comB[j];
         }
         double midComB = sumComB/comB.length;
-        System.out.println("Середній вік гравців команди \"Godzillas\": " + midComB + ".");
+        System.out.println("Середній вік гравців: " + midComB + ".");
     }
 }
 
